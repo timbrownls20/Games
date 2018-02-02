f8ef79bd82220f337a0a047d592e47ce0a244716
timbrownls20 <timbrownls26@hotmail.com>
2018-02-02 19:09:11 +1000

    implement routing

--- minesweeper/src/app/app.module.ts
+++ minesweeper/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { SettingsComponent } from './components/settings/settings.component';
+
+const appRoutes: Routes = [  
+  { path: 'game', component: GameComponent},
+  { path: 'settings', component: SettingsComponent},
+  { path: '', component: GameComponent } 
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
