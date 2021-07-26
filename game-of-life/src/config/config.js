71cb82d22eb3e53539f1c9d22a1605e139d28c65
timbrownls20 <timbrownls26@hotmail.com>
2021-07-26 16:46:28 +1000

    move game of life into own repo

--- game-of-life/src/config/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  Debug: false,
-  Css: {
-    Cell: "cell",
-    CellSelect: "cell-select",
-    CellOver: "cell-over",
-  },
-};
--- game-of-life/src/hooks/transformers/random.js
+++
@@ -1,9 +0,0 @@
-import mathsUtil from "../../utils/mathsUtil";
-
-  const random = (gridSettingState, gameStateDispatch) => {
-    let newRow = mathsUtil.random(1, gridSettingState.rows);
-    let newColumn = mathsUtil.random(1, gridSettingState.columns);
-    gameStateDispatch({ type: "cell-toggle", row: newRow, column: newColumn });
-  };
-
-  export default random;
